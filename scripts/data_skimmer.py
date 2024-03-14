b7d827ce5541cf1f7360c07a6e0867e977665e63
Noah Alexander Zipper <noah.alexander.zipper@cern.ch>
2024-03-14 18:59:40 +0100

    added useful scripts for working with files

---
+++ scripts/data_skimmer.py
@@ -0,0 +1,32 @@
+import ROOT
+import sys
+from os import path
+
+def main(bdt_cut=3, bmass_range_cut=(4.7, 5.7)):
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path_to_root_file>")
+        sys.exit(1)
+
+    root_file_path = sys.argv[1]
+
+    rdf = ROOT.RDataFrame("mytree", root_file_path)
+
+    xgb_cut = "bdt_score >= {}".format(bdt_cut)
+    bmass_cut = "(Bmass >= {}) && (Bmass <= {})".format(bmass_range_cut[0], bmass_range_cut[1])
+
+    rdf_filtered = rdf.Filter(xgb_cut).Filter(bmass_cut)
+
+    directory, filename = path.split(root_file_path)
+    base, extension = path.splitext(filename)
+    new_filename = "{}_{}{}".format(base, 'slimmed', extension)
+    updated_path = path.join(directory, new_filename)
+
+    rdf_filtered.Snapshot("mytree", updated_path)
+
+if __name__ == "__main__":
+    ROOT.EnableImplicitMT()
+
+    bdt_cut = 3.
+    bmass_range_cut = (4.7, 5.7)
+
+    main(bdt_cut, bmass_range_cut)
