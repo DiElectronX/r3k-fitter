e43da9ea20102c0e66f77f8713cf5a4231bb0f9f
Noah Alexander Zipper <noah.alexander.zipper@cern.ch>
2024-03-20 17:02:48 +0100

    fix combine fit script

--- scripts/runCombineFit.sh
+++ scripts/runCombineFit.sh
@@ -10,6 +10,7 @@ text2workspace.py $datacard
 combine -M MultiDimFit --saveWorkspace -n ".${label}" ${datacard/txt/root}
 combine -M FitDiagnostics "higgsCombine.${label}.MultiDimFit.mH120.root" -n ".${label}" --plots --saveShapes --signalPdfNames='*sig*' --backgroundPdfNames='*comb_bkg*,*part_bkg*' 
 
+mkdir -p combine_outputs
 mv "higgsCombine.${label}.MultiDimFit.mH120.root" ./combine_outputs
 mv "higgsCombine.${label}.FitDiagnostics.mH120.root" ./combine_outputs
 mv "fitDiagnostics.${label}.root" ./combine_outputs
